ccc9108bdc250c30f3066779ba5a012b9fa4411a
Nicholas Zaccardi <nick@skoshinteractive.com>
2014-07-19 23:23:00 -0500

    implemented the status property

--- src/gh/__init__.py
+++ src/gh/__init__.py
@@ -1,11 +1,59 @@
 import requests
 
+
 class Github(object):
 
-    def __init__(self, url="github.com"):
-        pass
+    # a flag for telling if the is an enterprise version of Github
+    _is_ghe = False
+
+    # github host url, used for Github Enterprise installations
+    host = "github.com"
+
+
+    def __init__(self, host=None):
+
+        if host is not None:
+            self._is_ghe = True
+            self.host = host
 
     @property
     def status(self):
-        return True
+        """Returns the up/down status of the API with a message"""
+
+        if self._is_ghe:
+            url = self._construct_url("")
+        else:
+            url = "https://status.github.com/api/status.json"
+
+        req = requests.get(url)
+
+        try:
+            status = req.json()
+        except ValueError:
+            return (False, "No JSON")
+
+        if req.status_code != 200:
+            return (False, "Not 200")
+
+        if not self._is_ghe and status['status'] != "good":
+            return (False, "API Down")
+
+        if self._is_ghe and "X-GitHub-Request-Id" not in req.headers:
+            return (False, "Bad GHE Server")
+
+        if not self._is_ghe and "x-octostatus-request-id" not in req.headers:
+            return (False, "Bad Github Server")
+
+        return (True, "All is well")
+
+    def _construct_url(self, endpoint, **kwargs):
+
+        endpoint = endpoint.lstrip("/").rstrip("/")
+
+        if self._is_ghe:
+            url_format = "https://{host}/api/v3/" + endpoint
+        else:
+            url_format = "https://api.{host}/" + endpoint
+
+        return url_format.format(host=self.host, endpoint=endpoint, **kwargs)
 
--- src/gh/tests/test_github.py
+++ src/gh/tests/test_github.py
@@ -1,6 +1,7 @@
 from gh import Github
 
 import unittest
+from nose.tools import eq_, ok_
 import requests
 
 def api_down():
@@ -14,10 +15,57 @@ def api_down():
 
 API_BAD = api_down()
 
-def test_pass_custom_url():
-    gh = Github("something.com")
+class TestGithub(unittest.TestCase):
 
-@unittest.skipIf(API_BAD, "Github API Down")
-def test_can_get_test_access_to_remote():
-    gh = Github()
-    assert gh.status
+    def test_pass_custom_url_for_ghe(self):
+        gh = Github("google.com")
+
+        assert gh.host == "google.com"
+        assert gh._is_ghe
+
+    def test_using_github_proper(self):
+        gh = Github()
+
+        assert gh.host == "github.com"
+        assert gh._is_ghe == False
+
+    @unittest.skipIf(API_BAD, "Github API Down")
+    def test_can_get_status_of_service(self):
+        gh = Github()
+        status, reason = gh.status
+
+        assert status
+        assert reason == "All is well"
+
+    def test_status_returns_false_when_service_doesnt_exist(self):
+        gh = Github("google.com")
+        status, reason = gh.status
+
+        ok_(not status)
+        eq_(reason, "No JSON")
+
+    def test_creating_an_api_endpoint_url(self):
+        gh = Github()
+
+        eq_(gh._construct_url("issues"), "https://api.github.com/issues")
+
+    def test_endpoint_url_deals_with_blank(self):
+        gh = Github()
+
+        eq_(gh._construct_url(""), "https://api.github.com/")
+
+    def test_can_pass_args_to_endpoint_url(self):
+        gh =Github()
+
+        eq_(gh._construct_url("users/{user}", user="nZac"),
+                "https://api.github.com/users/nZac")
+
+    def test_endpoint_strips_proper_slashes(self):
+        gh = Github()
+
+        eq_(gh._construct_url("/users/{user}", user="nZac"),
+                "https://api.github.com/users/nZac")
+        eq_(gh._construct_url("users/{user}/", user="nZac"),
+                "https://api.github.com/users/nZac")
+        eq_(gh._construct_url("/users/{user}/", user="nZac"),
+                "https://api.github.com/users/nZac")
